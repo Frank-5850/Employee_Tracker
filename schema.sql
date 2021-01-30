5de977b6dbaa9c8ef86e46a78858e8048bd13f42
Franco Phan <frank@Franks-MacBook-Pro.local>
2021-01-29 18:08:22 -0800

    Initialized inquirer, finished Schema.

--- schema.sql
+++ schema.sql
@@ -4,25 +4,26 @@ USE DATABASE employees_db;
 
 CREATE TABLE department (
     id INTEGER(11) AUTO_INCREMENT NOT NULL,
-    name VARCHAR(30),
+    name VARCHAR(30) NOT NULL,
     PRIMARY KEY (id)
 );
 
 CREATE TABLE role(
     id INTEGER AUTO_INCREMENT NOT NULL,
-    title VARCHAR(30) 
+    title VARCHAR(30) NOT NULL, 
+    salary DECIMAL(10,2) NOT NULL,
+    department_id INTEGER NOT NULL,
+    FOREIGN KEY(department_id) REFERENCES department(id),
+    PRIMARY KEY (id)
 );
-* **role**:
 
-  * **id** - INT PRIMARY KEY
-  * **title** -  VARCHAR(30) to hold role title
-  * **salary** -  DECIMAL to hold role salary
-  * **department_id** -  INT to hold reference to department role belongs to
-
-* **employee**:
-
-  * **id** - INT PRIMARY KEY
-  * **first_name** - VARCHAR(30) to hold employee first name
-  * **last_name** - VARCHAR(30) to hold employee last name
-  * **role_id** - INT to hold reference to role employee has
-  * **manager_id** - INT to hold reference to another employee that manages the employee being Created. This field may be null if the employee has no manager
+CREATE TABLE employee(
+    id INTEGER AUTO_INCREMENT NOT NULL,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INTEGER NOT NULL,
+    FOREIGN KEY (role_id) REFERENCES role(id),
+    manager_id INTEGER,
+    FOREIGN KEY (manager_id) REFERENCES manager(id),
+    PRIMARY KEY(id)
+);
