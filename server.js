53612fc2a36577a8bd5530fabfddce9310f7c0e1
Franco <phanfranco@yahoo.com>
2021-02-03 20:49:52 -0800

    Added switch case syntax.

--- server.js
+++ server.js
@@ -15,19 +15,51 @@ connection.connect((err) => {
 });
 
 const begin = () => {
-  inquirer.prompt({
-    name: "action",
-    type: "rawlist",
-    message: "What would you like to do?",
-    choices: [
-      "View all departments",
-      "View all roles",
-      "View all employees",
-      "Add a department",
-      "Add a role",
-      "Add a employee",
-      "Update employee",
-      "Exit",
-    ],
-  });
+  inquirer
+    .prompt({
+      name: "action",
+      type: "rawlist",
+      message: "What would you like to do?",
+      choices: [
+        "View all departments",
+        "View all roles",
+        "View all employees",
+        "Add a department",
+        "Add a role",
+        "Add a employee",
+        "Update employee",
+        "Exit",
+      ],
+    })
+    .then((answer) => {
+      switch (answer.action) {
+        case "View all departments":
+          break;
+
+        case "View all roles":
+          break;
+
+        case "View all employees":
+          break;
+
+        case "Add a department":
+          break;
+
+        case "Add a role":
+          break;
+
+        case "Add a employee":
+          break;
+
+        case "Update employee":
+          break;
+
+        case "exit":
+          break;
+
+        default:
+          console.log("That is not a valid choice");
+          break;
+      }
+    });
 };
